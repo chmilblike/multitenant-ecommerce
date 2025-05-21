95bc58b70ca3bc204b44c5004b95958fc15a3425
Gaoussou <coul.gaou@gmail.com>
2025-05-21 12:50:09 +0200

    01: setup

---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return <div className="text-rose-500 text">Hello World</div>
+}
