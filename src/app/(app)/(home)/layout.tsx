771ee221605f63ca2c10e0c7432487348832f861
Gaoussou <coul.gaou@gmail.com>
2025-05-22 14:19:14 +0200

    05: search filter

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,14 +1,40 @@
+import { Category } from '@/payload-types'
 import { Footer } from './footer'
 import { Navbar } from './navbar'
+import { SearchFilters } from './search-filters'
+
+import configPromise from '@payload-config'
+import { getPayload } from 'payload'
 
 interface Props {
   children: React.ReactNode
 }
 
-const Layout = ({ children }: Props) => {
+const Layout = async ({ children }: Props) => {
+  const payload = await getPayload({
+    config: configPromise,
+  })
+
+  const data = await payload.find({
+    collection: 'categories',
+    depth: 1, // Populate subcategories
+    pagination: false,
+    where: { parent: { exists: false } },
+  })
+
+  const formatedData = data.docs.map((doc) => ({
+    ...doc,
+    subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
+      /// Because of 'depth: 1' we are confident "doc" will be a type of "Category"
+      ...(doc as Category),
+      subcategories: undefined,
+    })),
+  }))
+
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
+      <SearchFilters data={formatedData} />
       <div className="flex-1 bg-[#f4f4f0]">{children}</div>
       <Footer />
     </div>
