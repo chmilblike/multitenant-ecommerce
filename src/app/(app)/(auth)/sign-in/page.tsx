79f9c1d42f7f6cd0f8b0f0322c3dd876f9fcbf73
Gaoussou <coul.gaou@gmail.com>
2025-05-26 14:33:45 +0200

    09: auth states

--- src/app/(app)/(auth)/sign-in/page.tsx
+++ src/app/(app)/(auth)/sign-in/page.tsx
@@ -1,7 +1,16 @@
 import { SignInView } from '@/modules/auth/server/ui/views/sign-in-view'
 import React from 'react'
 
-const Page = () => {
+import { caller } from '@/trpc/server'
+import { redirect } from 'next/navigation'
+
+const Page = async () => {
+	const session = await caller.auth.session()
+
+	if (session.user) {
+		redirect('/')
+	}
+
 	return <SignInView />
 }
 
