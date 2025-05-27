80a9bc57f16fd39b19144a587396f6ba7aff6e04
Gaoussou <coul.gaou@gmail.com>
2025-05-28 01:01:42 +0200

    16: tenant pages

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -2,7 +2,7 @@ import z from 'zod'
 
 import { baseProcedure, createTRPCRouter } from '@/trpc/init'
 import { Sort, Where } from 'payload'
-import { Category, Media } from '@/payload-types'
+import { Category, Media, Tenant } from '@/payload-types'
 import { sortValues } from '../search-params'
 import { DEFAULT_LIMIT } from '@/constants'
 
@@ -17,6 +17,7 @@ export const productsRouter = createTRPCRouter({
 				maxPrice: z.string().nullable().optional(),
 				tags: z.array(z.string()).nullable().optional(),
 				sort: z.enum(sortValues).nullable().optional(),
+				tenantSlug: z.string().nullable().optional(),
 			})
 		)
 		.query(async ({ ctx, input }) => {
@@ -42,6 +43,12 @@ export const productsRouter = createTRPCRouter({
 				}
 			} else if (input.maxPrice) {
 				where.price = { less_than_equal: input.maxPrice }
+			}
+
+			if (input.tenantSlug) {
+				where['tenant.slug'] = {
+					equals: input.tenantSlug,
+				}
 			}
 
 			if (input.category) {
@@ -89,7 +96,7 @@ export const productsRouter = createTRPCRouter({
 
 			const data = await ctx.db.find({
 				collection: 'products',
-				depth: 1, // Populate "category" & "image"
+				depth: 2, // Populate "category", "image" & tenant (& tenant.image)
 				where,
 				sort,
 				page: input.cursor,
@@ -101,6 +108,7 @@ export const productsRouter = createTRPCRouter({
 				docs: data.docs.map((doc) => ({
 					...doc,
 					image: doc.image as Media | null,
+					tenant: doc.tenant as Tenant & { image: Media | null },
 				})),
 			}
 		}),
