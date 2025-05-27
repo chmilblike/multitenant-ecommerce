dc5854b1efd3f28edb4a3acd9bc28e9d14054f8e
Gaoussou <coul.gaou@gmail.com>
2025-05-27 23:14:37 +0200

    13: multi tenancy

--- src/modules/home/ui/components/search-filters/subcategory-menu.tsx
+++ src/modules/home/ui/components/search-filters/subcategory-menu.tsx
@@ -7,10 +7,9 @@ import { useRouter } from 'next/navigation'
 interface Props {
 	category: CategoriesGetManyOutput[1]
 	isOpen: boolean
-	position: { top: number; left: number }
 }
 
-export const SubcategoryMenu = ({ category, isOpen, position }: Props) => {
+export const SubcategoryMenu = ({ category, isOpen }: Props) => {
 	const router = useRouter()
 	if (
 		!isOpen ||
@@ -23,10 +22,7 @@ export const SubcategoryMenu = ({ category, isOpen, position }: Props) => {
 	const backgroundColor = category.color || '#F5F5F5'
 
 	return (
-		<div
-			className="fixed z-100"
-			style={{ top: position.top, left: position.left }}
-		>
+		<div className="absolute z-100" style={{ top: '100%', left: 0 }}>
 			{/* Invisible bridge to maintain hover*/}
 			<div className="h-3 w-60" />
 			<div
