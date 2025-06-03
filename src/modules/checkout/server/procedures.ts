390088861d6707f3fb83a3c8283234a695fbb839
Gaoussou <coul.gaou@gmail.com>
2025-06-04 00:00:33 +0200

    26: general improvements

--- src/modules/checkout/server/procedures.ts
+++ src/modules/checkout/server/procedures.ts
@@ -75,6 +75,11 @@ export const checkoutRouter = createTRPCRouter({
 								equals: input.tenantSlug,
 							},
 						},
+						{
+							isArchived: {
+								not_equals: true,
+							},
+						},
 					],
 				},
 			})
@@ -179,9 +184,18 @@ export const checkoutRouter = createTRPCRouter({
 				collection: 'products',
 				depth: 2, // Populate "category", "image" & tenant (& tenant.image)
 				where: {
-					id: {
-						in: input.ids,
-					},
+					and: [
+						{
+							id: {
+								in: input.ids,
+							},
+						},
+						{
+							isArchived: {
+								not_equals: true,
+							},
+						},
+					],
 				},
 			})
 
